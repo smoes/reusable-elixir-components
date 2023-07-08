a1cba53c11c61c16b42e73bb2036dc637e60694c
Simon Härer <simon.haerer@bravobike.de>
2023-07-08 22:04:41 +0200

    Add non working counter

--- lib/demo_web/live/live_demo.ex
+++ lib/demo_web/live/live_demo.ex
@@ -4,15 +4,24 @@ defmodule DemoWeb.LiveDemo do
   alias DemoWeb.Components.Tabs
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    next_socket = socket |> assign(:counter, 0)
+    {:ok, next_socket}
   end
 
   def render(assigns) do
     ~H"""
     <.live_component module={Tabs} id="tabs">
-      <:tab label="Tab 1" id={:name_tab}>Tab 1</:tab>
+      <:tab label="Tab 1" id={:name_tab}><%= @counter %></:tab>
       <:tab label="Tab 2" id={:address_tab}>Tab 2</:tab>
     </.live_component>
+
+    <p phx-click="increment">Increment</p>
+    <p><%= @counter %></p>
     """
+  end
+
+  def handle_event("increment", _, socket) do
+    next_socket = socket |> update(:counter, fn counter -> counter + 1 end)
+    {:noreply, next_socket}
   end
 end
