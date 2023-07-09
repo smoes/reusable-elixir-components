f4e0ab0dbb388d025b6e5f77e2d44ee5d46ee8db
Simon Härer <simon.haerer@bravobike.de>
2023-07-09 21:50:17 +0200

    Add second coupled tabs

--- lib/demo_web/live/live_demo.ex
+++ lib/demo_web/live/live_demo.ex
@@ -9,10 +9,16 @@ defmodule DemoWeb.LiveDemo do
 
   def render(assigns) do
     ~H"""
-    <.live_component module={Tabs} id="tabs" tabs={@tabs}>
-      <:tab label="Tab 1" id={:name_tab}>Tab 1</:tab>
-      <:tab label="Tab 2" id={:address_tab}>Tab 2</:tab>
-    </.live_component>
+    <div class="flex space-y-10 flex-col">
+      <.live_component module={Tabs} id="tabs" tabs={@tabs}>
+        <:tab label="Tab 1" id={:name_tab}>Tab 1</:tab>
+        <:tab label="Tab 2" id={:address_tab}>Tab 2</:tab>
+      </.live_component>
+      <.live_component module={Tabs} id="tabs 2" tabs={@tabs}>
+        <:tab label="Tab 1" id={:name_tab}>Tab 1</:tab>
+        <:tab label="Tab 2" id={:address_tab}>Tab 2</:tab>
+      </.live_component>
+    </div>
     """
   end
 
